9b09263c4d58eac78c16f13bed5de2d141e72212
shuntaro <shun.sea.f@gmail.com>
2020-07-22 03:27:55 +0900

    追加実装

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,15 +1,15 @@
-class Prefecture < ActiveHash::Base
+class Drink < ActiveHash::Base
 
   self.data = [
-      {id: 1, name: '生ビール', price: 500, genre: 0}, {id: 2, name: '瓶ビール(大)' price: 600, genre: 0}, {id: 3, name: 'ホッピー' price:400 , genre: 1},
-      {id: 4, name: '黒ホッピー' price:400 , genre: 1}, {id: 5, name: 'バイスサワー' price:400 , genre: 1}, {id: 6, name: 'ウーロンハイ' price:400 , genre: 1},
-      {id: 7, name: '緑茶ハイ' price:400 , genre: 1}, {id: 8, name: 'レモンサワー' price:400 , genre: 1}, {id: 9, name: '青リンゴサワー' price:400 , genre: 1},
-      {id: 10, name: 'GFサワー' price:400 , genre: 1}, {id: 11, name: 'シークワーサーサワー' price:400 , genre: 1}, {id: 12, name: 'クエン酸サワー' price:400 , genre: 1},
-      {id: 13, name: 'ざくろサワー' price:400 , genre: 1}, {id: 14, name: 'ライムサワー' price:400 , genre: 1}, {id: 15, name: 'チューハイ' price:400 , genre: 1},
-      {id: 16, name: 'ウメサワー' price:400 , genre: 1}, {id: 17, name: '梅酒サワー' price:400 , genre: 1}, {id: 18, name: 'トマトハイ' price:400 , genre: 1},
-      {id: 19, name: '梅干し(1個)' price: 50, genre: 1}, {id: 20, name: '中' price: 220, genre: 1}, {id: 21, name: 'グラス(赤)' price: 350, genre: 2},
-      {id: 22, name: 'コーラ' price: 200, genre: 3}, {id: 23, name: 'ジンジャーエール' price: 200, genre: 3}, {id: 24, name: 'オレンジジュース' price: 200, genre: 3},
-      {id: 25, name: 'ウーロン茶' price: 200, genre: 3}
+      {id: 1, name: "ビール", drink_name: '生ビール', price: 500}, {id: 2, name: "ビール", drink_name: '瓶ビール(大)', price: 600}, {id: 3, name: "ソフトドリンク", drink_name: 'ホッピー', price:400},
+      {id: 4, name: "サワー類", drink_name: '黒ホッピー', price:400}, {id: 5, name: "サワー類", drink_name: 'バイスサワー', price:400}, {id: 6, name: "サワー類", drink_name: 'ウーロンハイ', price:400},
+      {id: 7, name: "サワー類", drink_name: '緑茶ハイ', price:400}, {id: 8, name: "サワー類", drink_name: 'レモンサワー', price:400}, {id: 9, name: "サワー類", drink_name: '青リンゴサワー', price:400},
+      {id: 10, name: "サワー類", drink_name: 'GFサワー', price:400}, {id: 11, name: "サワー類", drink_name: 'シークワーサーサワー', price:400}, {id: 12, name: "サワー類", drink_name: 'クエン酸サワー', price:400},
+      {id: 13, name: "サワー類", drink_name: 'ざくろサワー', price:400}, {id: 14, name: "サワー類", drink_name: 'ライムサワー', price:400}, {id: 15, name: "サワー類", drink_name: 'チューハイ', price:400},
+      {id: 16, name: "サワー類", drink_name: 'ウメサワー', price:400}, {id: 17, name: "サワー類", drink_name: '梅酒サワー', price:400}, {id: 18, name: "サワー類", drink_name: 'トマトハイ', price:400},
+      {id: 19, name: "サワー類", drink_name: '梅干し(1個)', price: 50}, {id: 20, name: "サワー類", drink_name: '中', price: 220}, {id: 21, name: "ワイン", drink_name: 'グラス(赤)', price: 350},
+      {id: 22, name: "ソフトドリンク", drink_name: 'コーラ', price: 200}, {id: 23, name: "ソフトドリンク", drink_name: 'ジンジャーエール', price: 200}, {id: 24, name: "ソフトドリンク", drink_name: 'オレンジジュース', price: 200},
+      {id: 25, name: "ソフトドリンク", drink_name: 'ウーロン茶', price: 200}
   ]
 
 end
